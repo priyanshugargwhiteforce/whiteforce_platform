a1a9de4294f50418aa9ac4f3824292c6ce0245a2
Prince Kashyap <princekashya252@gmail.com>
2026-08-12 18:30:17 +0530

    deployed in server

--- core/settings.py
+++ core/settings.py
@@ -133,7 +133,8 @@ USE_I18N = True
 USE_TZ = True
 
 # ── Static / Media ────────────────────────────────────────────────────────────
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
